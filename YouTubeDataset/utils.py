a23be716ad86227fdc39d24635eb01565ea92d6d
John Cumming <john@thesecretlocation.com>
2020-07-21 15:53:51 -0400

    Added support for decoders in ipd_display_dataset()

--- YouTubeDataset/utils.py
+++ YouTubeDataset/utils.py
@@ -14,10 +14,12 @@ import librosa.display as lrd
 from YouTubeDataset import YouTubeDataset
 
 
-def ipd_display_dataset(o, num_batches=1):
+def ipd_display_dataset(o, decoders=None, num_batches=1):
     """Displays a YouTubeDataset on all frontends.
     Parameters:
         o (YoutubeDataset or DataLoader): The YouTubeDataset or Dataloader(YouTubeDataset) to display
+        decoders {'FORMAT': (Callable), ...}: a Dictionary of decoders, one per dataset field type 
+        num_batches (int): the number of batches to display, default = 1
     Returns:
         handle (IPython.display.DisplayHandle): display handle
     """
@@ -40,6 +42,9 @@ def ipd_display_dataset(o, num_batches=1):
     if ds != None and dl != None:
         print(ds.fields)
         
+        if (decoders != None) and ('VIDEO' in ds.fields) and ('IMAGE' in decoders) and not ('VIDEO' in decoders):
+            decoders['VIDEO'] = lambda v: torch.stack([decoders['IMAGE'](f) for f in v])
+        
         ret = []
         
         for ix, batch in enumerate(dl):
@@ -57,21 +62,29 @@ def ipd_display_dataset(o, num_batches=1):
                 else:
                     print(k,t,s)
         
+                if decoders != None and k in decoders:
+                    if isinstance(dl, torch.utils.data.DataLoader): 
+                        v = [decoders[k](f) for f in v]
+                        if len(v) > 0 and isinstance(v[0], torch.Tensor):
+                            v = torch.stack(v)
+                    else:
+                        v = decoders[k](v)
+
                 if k == 'IMAGE':
-                    if len(v.shape) == 4: # (B, C, H, W)
+                    if isinstance(dl, torch.utils.data.DataLoader): 
                         plt.figure(figsize=(32,32))
                         plt.axis("off")
                         plt.title(k)
                         plt.imshow(np.transpose(torchvision.utils.make_grid(v, padding=2).cpu(),(1,2,0)))  
-                    elif len(v.shape) == 3: # (C,H,W)
+                    else:                                            # (C,H,W)
                         plt.figure(figsize=(2,2))
                         plt.axis("off")
                         plt.title(k)
                         plt.imshow(np.transpose(v.cpu(),(1,2,0)))  
                                         
                 if k == 'VIDEO':
-                    if len(v.shape) == 5:   # (B, T, H, W, C)
-                        v = v.view(v.shape[0]*v.shape[1], *v.shape[2:])
+                    if isinstance(dl, torch.utils.data.DataLoader):    # (B, T, H, W, C) => (T, H, W, C)
+                        v = v.reshape(v.shape[0]*v.shape[1], *v.shape[2:])
                     
                     if v.dtype != torch.uint8:
                         v = (v * 255).to(dtype=torch.uint8)
@@ -87,26 +100,33 @@ def ipd_display_dataset(o, num_batches=1):
                     finally:
                         os.remove(file)
                         
-                        
                 if k == 'AUDIO':
-                    if len(v.shape) == 3: # (B,K,L)
-                        fig = plt.figure()
-                        fig.set_figheight(v.shape[0])
-                        fig.set_figwidth(10)
-                        plt.title(k)
-                        for i, a in enumerate(v):
-                            if a.shape[0] == 1:
-                                a = a.view(a.shape[1])
-
-                            fig.add_subplot(v.shape[0]//2,2,i+1)
-                            lrd.waveplot(a.numpy(), sr=ds.audio_sr)
-                    elif len(v.shape) == 2: # (K,L)                        
-                        if v.shape[0] == 1:
-                            v = v.view(v.shape[1])
+                    if isinstance(dl, torch.utils.data.DataLoader): 
+                        # (B, K, L) => (K, L)
+                        # (B, K, F, T) ==> (K, F, T)
+                        v = v.permute(*range(1,len(v.shape)),0)
+                        v = v.reshape(*v.shape[:-2], np.prod(v.shape[-2:]))
                     
+                    if len(v.shape) == 2: # (K,L)  - Audio Tensor                      
+                        if v.shape[0] == 1:
+                            v = v.reshape(v.shape[0]*v.shape[1])
+                        
+                        n = np.asfortranarray(v.numpy())
                         plt.figure(figsize=(5, 2))
                         plt.title(k)
-                        lrd.waveplot(v.numpy(), sr=ds.audio_sr)
+                        lrd.waveplot(n, sr=ds.audio_sr)
+                        ret.append(ipd.Audio(n, rate=ds.audio_sr))
+
+                    elif len(v.shape) == 3: # (K, F, T) - Mel Spectrogram Tensor
+                        plt.figure(figsize=(10, 4))
+                        plt.title(k)
+                        lrd.specshow(v[0].numpy(), y_axis='linear', hop_length=200, x_axis='time', sr=ds.audio_sr)
+                        
+                if k == 'TEXT':
+                    if isinstance(v, str):
+                        print('TEXT', v)
+                    elif ((isinstance(v, list) or isinstance(v, tuple)) and isinstance(v[0], str)):
+                        print('TEXT', v)
 
             if (ix+1) == num_batches:
                 return ipd.display(*ret)
